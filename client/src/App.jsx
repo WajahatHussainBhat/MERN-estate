c7e63001d92222f10c190d3d0b1c4ba78e370466
Wajahat <wajahathussainbhat@gmail.com>
2023-10-09 02:27:42 +0530

    create and run

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,30 +4,32 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
-
+import Header from "./components/Header";
 
 const App = () => {
     return ( <
         BrowserRouter >
         <
+        Header / >
+        <
         Routes >
         <
         Route path = "/"
         element = { < Home / > }
-        /> <
+        />{" "} <
         Route path = "/sign-in"
         element = { < SignIn / > }
-        /> <
+        />{" "} <
         Route path = "/sign-out"
         element = { < SignUp / > }
-        /> <
+        />{" "} <
         Route path = "/profile"
         element = { < Profile / > }
-        /> <
+        />{" "} <
         Route path = "/about"
         element = { < About / > }
-        /> <
-        /Routes> <
+        />{" "} <
+        /Routes>{" "} <
         /BrowserRouter>
     );
 };
